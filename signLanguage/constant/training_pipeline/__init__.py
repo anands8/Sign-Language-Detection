74ab351efdaa3049eb0b4dbf379fe0fb9c2bc551
anands8 <anand.s8@outlook.com>
2023-04-15 17:06:17 +0530

    Data ingestion added

---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+ARTIFACTS_DIR: str = "artifacts"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://github.com/anands8/Sign-Language-Detection/blob/main/Sign_language_data.zip"
