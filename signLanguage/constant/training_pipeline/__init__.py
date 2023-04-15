a0ea23af07ea65b164262e52a7e164ae05ffb97b
anands8 <anand.s8@outlook.com>
2023-04-15 17:17:50 +0530

    Link corrected

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -10,4 +10,4 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/anands8/Sign-Language-Detection/blob/main/Sign_language_data.zip?raw=true"
+DATA_DOWNLOAD_URL: str = "https://raw.githubusercontent.com/anands8/Sign-Language-Detection/main/Sign_language_data.zip"
